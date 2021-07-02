567b7cdeba4d3556eb6cf74e8e88b65ffe2d019c
satoshi_MTS <hannnari0918@gmail.com>
2021-07-02 20:26:41 +0900

    First submit

---
+++ Problem_2.py
@@ -0,0 +1,19 @@
+# 紙を1回折った時の厚さを計算するコード
+THICKNESS = 0.00008
+
+# 紙を2回折った時の厚さを計算するコード
+# folded_thickness = THICKNESS * 2
+
+# 43回の場合
+number_of_folding = 43
+
+folded_thickness = THICKNESS * (2 ** number_of_folding)
+
+print("厚さ： {}メートル".format(folded_thickness))
+
+# メートルをキロメートルに変換して小数点以下2桁で表示する
+# print("厚さ： {:.2f}キロメートル".format(folded_thickness / 1000))
+
+# メートルを万キロメートルに変換
+print('厚さ : {:.2f}万キロメートル'.format(folded_thickness / 10 ** 7))
+
